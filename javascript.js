fce05a03b1bc8acbdb783b3a0d2665fdd31a8d99
dianaz <stingray2091@gmail.com>
2020-05-02 18:57:56 -0400

    made the save button clickable and set to the time

--- javascript.js
+++ javascript.js
@@ -9,12 +9,14 @@ hours.forEach(element => {
     // created row description for input
     var inputField = $('<td>');
     var input = $('<input>');
+    input.attr("id", element);
     inputField.append(input);
 
     // created row descripton for button 
     var saveButton = $('<td>');
     var btn = $('<button>');
     btn.attr("class", "saveBtn")
+    btn.attr("id", element);
 
     saveButton.append(btn)
 
@@ -23,8 +25,22 @@ hours.forEach(element => {
 
 });
 
-$(".saveBtn").on('click', function () {
-    console.log("hello")
-})
+$(".saveBtn").on('click', function (e) {
 
-// event lister click when click on a button how to get the table description before it. how to select the input field to save the different data. could have class like time input button. or jquery.    
+    currentButton = e.target;
+
+    console.log(currentButton)
+
+    var buttonId = currentButton.getAttribute("id");
+
+    console.log(buttonId)
+
+
+
+    //sessionStorage.setItem(JSON.stringify());
+
+})
+$('#currentDay').text(moment().format('MMMM Do YYYY'));
+
+
+// event lister click when click on a button how to get the table description before it. how to select the input field to save the different data. could have class like time input button. or jquery.
